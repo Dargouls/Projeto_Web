2c46eff526f8b8ddc4324f0aba3760a15773ea6e
Gabriel Azevedo <gah_500@hotmail.com>
2020-12-02 14:27:05 -0300

    consertado a chamada da função e o campo email não aparecia

--- Scripts/cadastro.js
+++ Scripts/cadastro.js
@@ -1,17 +1,17 @@
 
-placeholderEmail();
+placeholders();
 
 function placeholders (){
 	var larguraTela = screen.width;
 	if (larguraTela <= "992"){
-		var placeholderNome = document.querySelector("#nome");
-		placeholderNome.placeholder = "NOME";
+		var placeholderNome = document.querySelector("#input-nome");
+		placeholderNome.placeholder = "Nome de usuário (4-12 caracteres)";
 
-		var placeholderEmail = document.querySelector("#email");
-		placeholderEmail.placeholder = "EMAIL";
+		var placeholderEmail = document.querySelector("#input-email");
+		placeholderEmail.placeholder = "E-mail";
 
-		var placeholderSenha = document.querySelector("#senha");
-		placeholderSenha.placeholder = "SENHA";
+		var placeholderSenha = document.querySelector("#input-senha");
+		placeholderSenha.placeholder = "Senha";
 	}
 	
 }
