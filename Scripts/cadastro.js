e08949a55345506e5f39346dcb426149f7e82d2f
Gabriel Azevedo <gah_500@hotmail.com>
2020-12-06 13:58:45 -0300

    Consertada a animação do conteúdo do botão mobile

--- Scripts/cadastro.js
+++ Scripts/cadastro.js
@@ -1,11 +1,13 @@
 
+var nomeForm = document.getElementById("input-nome");
+var emailForm = document.getElementById("input-email");
+var senhaForm = document.getElementById("input-senha");
+
 placeholders();
 
 function validar(){
-    var nomeForm = document.getElementById("input-nome");
-    var emailForm = document.getElementById("input-email");
-	var senhaForm = document.getElementById("input-senha");
-	var sexoForm = document.getElementById("input-sexo")
+    
+	var sexoForm = document.getElementById("input-sexo");
 	var erro = document.getElementById("erro-preencher");
 
     if(nomeForm.value == "" || emailForm.value == "" || senhaForm.value == "" || sexoForm.value == "valor-padrao"){
@@ -22,13 +24,10 @@ function validar(){
 function placeholders (){ //Mudar o conteúdo dos placeholders no HTML para dispositivos menores
 	var larguraTela = screen.width;
 	if (larguraTela <= "992"){
-		var placeholderNome = document.querySelector("input-nome");
-		placeholderNome.placeholder = "Nome de usuário (4-12 caracteres)";
+		nomeForm.placeholder = "Nome de usuário (4-12 caracteres)";
 
-		var placeholderEmail = document.querySelector("input-email");
-		placeholderEmail.placeholder = "E-mail";
+		emailForm.placeholder = "E-mail";
 
-		var placeholderSenha = document.querySelector("input-senha");
-		placeholderSenha.placeholder = "Senha (6-12 caracteres)";
+		senhaForm.placeholder = "Senha (6-12 caracteres)";
 	}
 }
