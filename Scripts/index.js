3bb8d843bd9951e5a93259e1c82ccc467fa1f2b1
Gabriel Azevedo <gah_500@hotmail.com>
2020-12-28 14:19:55 -0300

    Mudança visual no pré-visualizador do arquivo e redirecionamento para a página de leitura

--- Scripts/index.js
+++ Scripts/index.js
@@ -25,14 +25,15 @@ function mostrarArquivo(){
   lerPDF.style = "border-top-right-radius: 0; border-bottom-right-radius: 0; border-right: none;";
   arquivoSelecionado.style = 'width: 400px';
   arquivoSelecionado.removeAttribute("disabled");
-  arquivoSelecionado.innerHTML = "<p>"+nome+"</p>";
+  arquivoSelecionado.innerHTML = "<p>"+nome+"</p>"+'<span class="referencia">Está certo? Clique para continuar!</span>';
 }
 function esconderArquivo(){
   arquivoSelecionado.style.width = '0px';
   arquivoSelecionado.setAttribute("disabled");
 }
 function lerArquivo(){
-  botoesArquivoDIV.style = "transform: scale(1.2);"
-  arquivoSelecionado.style = " width: 400px; transform: scale(1.1) translateX(-140px); box-shadow: 0px 0px 250px 300px rgba(0, 0, 0, 0.7);"
+  botoesArquivoDIV.style = "transform: scale(1.2); background-color: rgba(0, 0, 0, 0.7); box-shadow: 0px 0px 250px 300px rgba(0, 0, 0, 0.7);";
+  arquivoSelecionado.style = " width: 400px; transform: scale(1.1) translateX(-140px);"
   lerPDF.style = "transform: translateX(200px);"
+  setTimeout(function() {window.location.href = "leitura.html"}, 1000);
 }
